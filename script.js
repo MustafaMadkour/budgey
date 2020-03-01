e3fd44bac42ecf689249b533bf05b8163688f287
Mustafa Madkour <madkour2790@gmail.com>
2020-03-01 08:45:10 +0200

    basic structure for app architecture

---
+++ script.js
@@ -0,0 +1,15 @@
+// budget controller module
+var budgetController = (function() {
+    
+})();
+
+
+// UI controller module
+var uiController = (function() {
+
+})();
+
+// app controller module
+var controller = (function(budgCtrl, uiCtrl) {
+
+})(budgetController, uiController);
