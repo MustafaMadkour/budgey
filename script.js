95dd26585b266087afec01e4e276929def58f637
Mustafa Madkour <madkour2790@gmail.com>
2020-03-08 21:37:11 +0200

    modify date format

--- script.js
+++ script.js
@@ -198,7 +198,7 @@ var UIController = (function () {
                 type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: parseFloat(document.querySelector(DOMstrings.inputValue).value),
-                time: document.querySelector(DOMstrings.inputTime).value=x
+                time: document.querySelector(DOMstrings.inputTime).value=x.getDate()+'.'+(x.getMonth()+1)+'.'+x.getUTCFullYear()+', '+x.getHours()+':'+x.getMinutes()
             };
         },
 
@@ -210,11 +210,11 @@ var UIController = (function () {
             if (type === 'inc') {
                 element = DOMstrings.incomeContainer;
 
-                html = '<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description% </div> <div class="item__time"><span> in</span> : %time%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                html = '<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description% </div> </br><div class="item__time"><span></span> : %time%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             } else if (type === 'exp') {
                 element = DOMstrings.expensesContainer;
 
-                html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div> <div class="item__time"><span> in</span> : %time%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div> </br><div class="item__time"><span></span> : %time%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
 
             // Replace the placeholder text with some actual data
